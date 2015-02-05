5ba9a9df0c544c21b4e7dfe2cbf835e1ad85cfd4
joeper214 <Joeper.serrano@cloudsherpas.com>
2015-02-05 23:11:16 +0800

    added input validators on allocate modal

--- angular-app/controllers/alloc_controller.js
+++ angular-app/controllers/alloc_controller.js
@@ -13,28 +13,60 @@ appControllers.controller('allocateCtrl', function ($scope, $modalInstance, item
 		
 	    });	
     };
+    //Configurations for datepicker angular bootstrap
+    
+    $scope.open = function($event) {
+	$event.preventDefault();
+	$event.stopPropagation();
+	
+	$scope.opened = true;
+    };
+    
+    $scope.formats = ['dd-MMMM-yyyy','MM/dd/yyyy', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[1];
+
+
 
     //my todo app for testing
     $scope.resources = [];
     $scope.hours = [];
     $scope.dates = [];
+    $scope.disp_dates = [];
     $scope.addTodo = function () {
-      $scope.resources.push($scope.resource);
-	
-      $scope.hours.push($scope.hour);
-      $scope.dates.push($scope.date);
-	$scope.resource = '';
-	$scope.hour = '';
+	if($scope.hour == null || $scope.hour == ''){
+	    $("#hour_err").focus();
+	    //setTimeout( $("#hour_err").hide(), 3000);
+	}else if($scope.resource == null || $scope.resource == ''){
+	    $("#resource_err").focus();
+	    //setTimeout( $("#resource_err").hide(), 3000);
+	}else if($scope.disp_date == null || $scope.disp_date == ''){
+	    $("#dateString").focus();
+	}else{
+	    $scope.resources.push($scope.resource);
+	    var dateString = $('#dateString').val();
+	    var timestamp = Date.parse(dateString).getTime()/1000;
+	    $scope.date = timestamp;
+	    $scope.hours.push($scope.hour);
+	    $scope.dates.push($scope.date);
+	    $scope.disp_dates.push($scope.disp_date);
+	    $scope.resource = null;
+	    $scope.hour = null;
+	    $scope.date = '';
+	    $scope.disp_date = null;
+	}
     };
     
     $scope.removeTodo = function (index) {
       $scope.resources.splice(index, 1);
       $scope.hours.splice(index, 1);
+      $scope.dates.splice(index, 1);
+      $scope.disp_dates.splice(index, 1);
     };
 
     $scope.allocation['alloc_hours'] = $scope.hours;
     $scope.allocation['resource_name'] = $scope.resources;
-   
+    $scope.allocation['alloc_date'] = $scope.dates;
+    
     
     $scope.ok = function(){
 	console.log($scope.selected['project_id'].key);    
