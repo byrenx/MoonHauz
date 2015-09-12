8a8492c6f06c1cf5633b198229134cb2876bca25
byrenx <bayron.jofel@gmail.com>
2015-09-13 00:51:24 +0800

    initial property listing design page

--- angular-app/app/routes.js
+++ angular-app/app/routes.js
@@ -1,22 +1,27 @@
 (function(angular){
 
-  "use restrict";
+    "use restrict";
 
-  angular.module('app')
+    angular.module('app')
 	.config(routes);
 
-  routes.$inject = ['$routeProvider', '$locationProvider'];
+    routes.$inject = ['$routeProvider', '$locationProvider'];
 
-  function routes(routeProvider, locationProvider){
-	   routeProvider
-	     .when('/home',{
-    		  templateUrl : 'ng/templates/guest-home.html',
-    		  controller  : '',
-    		  controllerAs: '',
-	     })
-	     .otherwise({
+    function routes(routeProvider, locationProvider){
+	routeProvider
+	    .when('/home',{
+    		templateUrl : 'ng/templates/guest-home.html',
+    		controller  : '',
+    		controllerAs: '',
+	    })
+	    .when('/properties', {
+		templateUrl : 'ng/templates/app-properties-list.html',
+    		controller  : '',
+    		controllerAs: '',
+	    })
+	    .otherwise({
 	        redirectTo : '/home',
-	     });
-  }
+	    });
+    }
 
 })(window.angular);
